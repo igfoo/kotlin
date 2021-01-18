fd807d660fb6b8aecf1a92040b5f370860c3ea70
Anastasiya Shadrina <a.shadrina5@mail.ru>
2021-01-18 19:47:07 +0700

    [Parse, FE, PSI2IR] Use labels for referencing specific receiver

--- compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/generators/FunctionGenerator.kt
+++ compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/generators/FunctionGenerator.kt
@@ -48,7 +48,7 @@ class FunctionGenerator(declarationGenerator: DeclarationGenerator) : Declaratio
         declareSimpleFunction(
             ktFunction,
             ktFunction.receiverTypeReference,
-            ktFunction.contextReceiverTypeReferences,
+            ktFunction.contextReceivers.mapNotNull { it.typeReference() },
             origin,
             getOrFail(BindingContext.FUNCTION, ktFunction)
         ) {
@@ -124,7 +124,7 @@ class FunctionGenerator(declarationGenerator: DeclarationGenerator) : Declaratio
             irAccessor.returnType = irAccessor.descriptor.returnType!!.toIrType()
             generateValueParameterDeclarations(
                 irAccessor, ktAccessor ?: ktProperty, ktProperty.receiverTypeReference,
-                ktProperty.contextReceiverTypeReferences
+                ktProperty.contextReceivers.mapNotNull { it.typeReference() }
             )
             val ktBodyExpression = ktAccessor?.bodyExpression
             irAccessor.body =
@@ -235,7 +235,7 @@ class FunctionGenerator(declarationGenerator: DeclarationGenerator) : Declaratio
         declareConstructor(
             ktClassOrObject,
             ktClassOrObject.primaryConstructor ?: ktClassOrObject,
-            ktClassOrObject.contextReceiverTypeReferences,
+            ktClassOrObject.contextReceivers.mapNotNull { it.typeReference() },
             primaryConstructorDescriptor
         ) { irConstructor ->
             if (
@@ -252,7 +252,7 @@ class FunctionGenerator(declarationGenerator: DeclarationGenerator) : Declaratio
         return declareConstructor(
             ktConstructor,
             ktConstructor,
-            ktClassOrObject.contextReceiverTypeReferences,
+            ktClassOrObject.contextReceivers.mapNotNull { it.typeReference() },
             constructorDescriptor
         ) {
             when {
