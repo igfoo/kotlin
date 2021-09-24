f430e7598fe4b217cc5544534e48b2ec5a49900d
Tianyu Geng <tgeng@google.com>
2021-09-24 11:43:02 -0700

    FIR checker: refactor FirSyntaxChecker
    Introduce a new checkPsiOrLightTree so the structure is more symmetric.
    In addition, now implementation of `checkLightTree` has access to
    `FirLightSourceElement` rather tha a generic `FirSourceElement`

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/extended/RedundantVisibilityModifierSyntaxChecker.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/extended/RedundantVisibilityModifierSyntaxChecker.kt
@@ -35,7 +35,7 @@ import org.jetbrains.kotlin.psi.KtDeclaration
 
 object RedundantVisibilityModifierSyntaxChecker : FirDeclarationSyntaxChecker<FirDeclaration, KtDeclaration>() {
 
-    override fun checkLightTree(
+    override fun checkPsiOrLightTree(
         element: FirDeclaration,
         source: FirSourceElement,
         context: CheckerContext,
